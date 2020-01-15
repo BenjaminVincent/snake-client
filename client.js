b7a04f9dfc0d572e5eef23c4ccbf38770f23146b
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-15 20:46:13 +0000

    added name to client

--- client.js
+++ client.js
@@ -1,9 +1,23 @@
 const net = require('net');
 
+
+/*
+  Estabilishes connection with the game server
+
+*/
+
 const connect = function() {
   const conn = net.createConnection({
     host: '10.0.13.4',
     port: 50541
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('connect', () =>{
+    conn.write('Name: BEN');
   });
 
   conn.setEncoding('utf8');
--- play.js
+++ play.js
@@ -1,10 +1,7 @@
 const net = require('net');
 const { connect } = require('./client');
 
-/*
-  Estabilishes connection with the game server
 
-*/
 
 
 
