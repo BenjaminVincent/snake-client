c2593a1d7edb6ade9c21a1087f25349ea416de8b
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-15 20:19:01 +0000

    added client.js and play.js

---
+++ play.js
@@ -0,0 +1,13 @@
+const net = require('net');
+const { connect } = require('./client');
+
+/*
+  Estabilishes connection with the game server
+
+*/
+
+
+
+
+console.log('Connecting ...');
+connect();
